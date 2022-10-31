959e285ab885ffa1b2d1df39698e446fcdefa05a
AlexFrid <post@alexanderfridriksson.com>
2022-10-31 22:07:45 +0000

    changed social icons to match both header and footer

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,20 @@
 
 export const SITE_TITLE = 'Data Story Design';
 export const SITE_DESCRIPTION = 'Learn with me about data, storytelling, and design';
+export const socials = [
+    {
+        name: 'Github',
+        url: 'https://github.com/AlexFrid',
+        icon: "mdi:github",
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/alexanderfridriksson',
+        icon: "mdi:linkedin",
+    },
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/DataStoryDesign',
+        icon: "mdi:twitter",
+    },
+]
