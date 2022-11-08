e440a33f93cf7bb9db0c91b68ec04bf1d205fcc6
AlexFrid <post@alexanderfridriksson.com>
2022-11-08 20:52:27 +0000

    Made adjustments for mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			'display': ['Caveat', 'sans-serif']
+			'display': ['Caveat', 'system-ui', 'sans-serif'],
+			'sans': ['roboto-flex', 'system-ui', 'sans-serif'],
 		},
 		extend: {},
 	},
